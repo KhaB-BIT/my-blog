8be47cd118f770bf201a93ab37a555ca7ba233ff
Minh Kha Bui <buiminhkha24072001@gmail.com>
2023-05-29 22:24:56 +0700

    commit

--- src/component/page/homepage/Home.jsx
+++ src/component/page/homepage/Home.jsx
@@ -1,4 +1,4 @@
-import React from "react"
+import React, { useMemo } from "react"
 import { NavLink } from "react-router-dom"
 import NavBar from "./NavBar"
 import AboutBlog from "./AboutBlog"
@@ -9,6 +9,28 @@ import OtherImg from "../../images/other.webp"
 import Snow from "./Snow"
 
 const Home = () => {
+    const sources = useMemo(() => {
+        return [
+            {
+                url: "/java",
+                src: JavaImg,
+                alt: "Source Java",
+                title: "Java Basic",
+            },
+            {
+                url: "/springboot",
+                src: SpringbootImg,
+                alt: "Source Springboot",
+                title: "Spring Boot Basic",
+            },
+            {
+                url: "/",
+                src: OtherImg,
+                alt: "Tương lai sẽ làm...",
+                title: "Tương lai sẽ làm...",
+            },
+        ]
+    }, [])
     return (
         <div>
             <Snow />
@@ -17,40 +39,22 @@ const Home = () => {
             <div className="w-full lg:w-[60vw] px-[15px] m-auto">
                 <AboutBlog />
                 <div className="flex flex-wrap justify-around">
-                    <NavLink to="/java">
-                        <div className="group flex flex-col m-3 rounded-lg overflow-hidden shadow-lg shadow-indigo-500/40 cursor-pointer hover:opacity-90">
-                            <img
-                                src={JavaImg}
-                                alt="Java"
-                                className="w-[260px] h-[150px] object-cover hover:scale-110 transition-all duration-300 ease-linear"
-                            />
-                            <div className="text-lg p-2">Java Basic</div>
-                        </div>
-                    </NavLink>
-
-                    <NavLink to="/springboot">
-                        <div className="flex flex-col m-3 rounded-lg overflow-hidden shadow-lg shadow-indigo-500/40 cursor-pointer hover:opacity-90">
-                            <img
-                                src={SpringbootImg}
-                                alt="Springboot"
-                                className="w-[260px] h-[150px] object-cover hover:scale-110 transition-all duration-300 ease-linear"
-                            />
-                            <div className="text-lg p-2">Spring Boot</div>
-                        </div>
-                    </NavLink>
-
-                    <NavLink to="/">
-                        <div className="flex flex-col m-3 rounded-lg overflow-hidden shadow-lg shadow-indigo-500/40 hover:opacity-90">
-                            <img
-                                src={OtherImg}
-                                alt="OtherImg"
-                                className="w-[260px] h-[150px] object-cover hover:scale-110 transition-all duration-300 ease-linear"
-                            />
-                            <div className="text-lg p-2">
-                                Tương lai sẽ làm...
-                            </div>
-                        </div>
-                    </NavLink>
+                    {sources.map((item) => {
+                        return (
+                            <NavLink to={item.url}>
+                                <div className="group flex flex-col m-3 rounded-lg overflow-hidden shadow-lg shadow-indigo-500/40 cursor-pointer hover:opacity-90">
+                                    <img
+                                        src={item.src}
+                                        alt={item.alt}
+                                        className="w-[320px] lg:w-[270px] h-[150px] object-cover hover:scale-110 transition-all duration-300 ease-linear"
+                                    />
+                                    <div className="text-lg p-2">
+                                        {item.title}
+                                    </div>
+                                </div>
+                            </NavLink>
+                        )
+                    })}
                 </div>
             </div>
 
